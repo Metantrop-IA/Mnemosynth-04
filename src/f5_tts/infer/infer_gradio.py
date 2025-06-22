02cd7d4ee94361643f7edb59961f3c70c1f60f49
Alan Turing <metantropia.msg@gmail.com>
2025-06-21 22:15:32 -0600

    Commit

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -408,7 +408,7 @@ with gr.Blocks() as app_credits:
 * [ΜΕΤΑΝΘΡΩΠΙΑ](https://github.com/METANTROP-IA) by the [Mnemosynth demo].(https://github.com/Metantrop-IA/Mnemosynth-01)
 * [OpenAI](https://huggingface.co/openai) by Whisper.
 * [Alibaba](https://huggingface.co/Qwen) by Qwen.                     
-* [Yushen Chen](https://huggingface.co/SWivid) by the original F5-TTS paper].(https://arxiv.org/abs/2410.06885)
+* [Yushen Chen](https://huggingface.co/SWivid) by the original [F5-TTS paper].(https://arxiv.org/abs/2410.06885)
 * [jpgallegoar](https://github.com/jpgallegoar) by the Spanish fine-tuning of F5-TTS model.
                 
 """)
