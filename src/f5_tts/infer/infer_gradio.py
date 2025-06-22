02adf8613506a7482644305b47d228e56e2239dd
Alan Turing <metantropia.msg@gmail.com>
2025-06-21 22:34:02 -0600

    Commit

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -405,10 +405,10 @@ The Memory Synthesizer {read_personality_file()}
 with gr.Blocks() as app_credits:
     gr.Markdown("""
 # Credits
-* [ΜΕΤΑΝΘΡΩΠΙΑ](https://github.com/METANTROP-IA) by the [Mnemosynth demo].(https://github.com/Metantrop-IA/Mnemosynth-01)
+* [ΜΕΤΑΝΘΡΩΠΙΑ](https://github.com/METANTROP-IA) by the [Mnemosynth demo.](https://github.com/Metantrop-IA/Mnemosynth-01)
 * [OpenAI](https://huggingface.co/openai) by Whisper.
 * [Alibaba](https://huggingface.co/Qwen) by Qwen.                     
-* [Yushen Chen](https://huggingface.co/SWivid) by the original [F5-TTS paper].(https://arxiv.org/abs/2410.06885)
+* [Yushen Chen](https://huggingface.co/SWivid) by the original [F5-TTS paper.](https://arxiv.org/abs/2410.06885)
 * [jpgallegoar](https://github.com/jpgallegoar) by the Spanish fine-tuning of F5-TTS model.
                 
 """)
